ae33714b36856a0931017cc8cbbc70fde9ac0be4
John Poet <jpoet@mythtv.org>
2019-01-08 17:13:14 -0700

    move audio_CS8416 initialization inside try block as suggested by Steven Viola
    Some HD-PVR2 do not have the audio_CS8416 part, and an exception will
    be thrown on initialization.

--- HauppaugeDev.cpp
+++ HauppaugeDev.cpp
@@ -138,8 +138,6 @@ bool HauppaugeDev::set_input_format(encoderSource_t source,
         (m_params.audioCodec == HAPI_AUDIO_CODEC_AC3 ? ENCAIF_AC3
          : ENCAIF_AUTO);
 
-    audio_CS8416 audio_CS8416(*m_fx2);
-
     if (spdif)
     {
         // check that CS8416 part is installed and supported by this device
@@ -147,6 +145,7 @@ bool HauppaugeDev::set_input_format(encoderSource_t source,
         {
             // init CS8416 optical receiver if detected
             // switch audio mux to SPDIF
+            audio_CS8416 audio_CS8416(*m_fx2);
             audio_CS8416.reset(audio_CS8416::AudioInput::OPTICAL);
 
             m_fx2->setPortStateBits(FX2_PORT_E, 0x10, 0x00);
@@ -171,15 +170,17 @@ bool HauppaugeDev::set_input_format(encoderSource_t source,
                 {
                     try
                     {
-                        m_fx2->setPortStateBits(FX2_PORT_E, 0x10, 0x00);
+                        audio_CS8416 audio_CS8416(*m_fx2);
                         audio_CS8416.reset(audio_CS8416::AudioInput::HDMI);
+
+                        m_fx2->setPortStateBits(FX2_PORT_E, 0x10, 0x00);
                         LOG(Logger::NOTICE) << "'SPDIF' from HDMI via 8416"
                                             << flush;
                     }
                     catch (std::runtime_error &e)
                     {
                         LOG(Logger::WARNING) << "Can't initialize CS8416 part. "
-                            "S/PDIF input won't be available" << flush;
+                            "AC3 codec will not be available" << flush;
                         spdif = false;
                     }
                 }
