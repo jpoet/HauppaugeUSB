jpoet/HauppaugeUSB

commit a88c865f0b535517070c90a30ca10835dc1ba661

Fix using a config file without mythtv. Fix sending to stdout when neither and output or mythtv is specified.

hauppauge2.cpp
@@ -301,7 +301,9 @@ int main(int argc, char *argv[])
 
     notify(vm);
 
+#if 0
     InitInterruptHandler();
+#endif
 
     // Display help text when requested
     if (vm.count("help"))
@@ -412,67 +414,67 @@ int main(int argc, char *argv[])
     if (vm.count("aspect"))
         params.aspectRatio = vm["aspect"].as<float>();
 
-    params.mythtv = (vm.count("mythtv"));
+    params.mythtv = (vm.count("mythtv")) ? vm["mythtv"].as<bool>() : false;
 
-    if (vm.count("output") || params.mythtv)
+    if (vm.count("output"))
+        params.output = vm["output"].as<string>();
+    else
+        params.output = "stdout";
+
+    if (params.mythtv)
     {
-        if (params.mythtv)
+        string desc = vm.count("description") ?
+                      vm["description"].as<string>() : params.serial;
+        MythTV mythtv(params, desc);
+        mythtv.Wait();
+    }
+    else
+    {
+        HauppaugeDev dev(params);
+        if (!dev)
+            return -2;
+
+        USBWrapper_t usbio;
+        if (!usbio.Open(params.serial))
+            return -3;
+
+        if (!dev.Open(usbio, (params.audioCodec == HAPI_AUDIO_CODEC_AC3)))
         {
-            string desc = vm.count("description") ?
-                          vm["description"].as<string>() : params.serial;
-            MythTV mythtv(params, desc);
-            mythtv.Wait();
+            usbio.Close();
+            return -4;
+        }
+
+        if (vm.count("duration") && vm["duration"].as<int>() > 0)
+        {
+            if (dev.StartEncoding())
+            {
+                chrono::steady_clock::time_point start =
+                    chrono::steady_clock::now();
+                std::chrono::seconds elapsed = chrono::seconds(0);
+                std::chrono::seconds duration =
+                    chrono::seconds(vm["duration"].as<int>());
+                do
+                {
+                    elapsed = chrono::duration_cast<chrono::seconds>
+                              (chrono::steady_clock::now() - start);
+                    PrintPosition(elapsed, duration);
+                    std::this_thread::sleep_for(std::chrono::seconds(1));
+                }
+                while (elapsed.count() < duration.count());
+                cerr << "\n\n";
+                dev.StopEncoding();
+            }
         }
         else
         {
-            params.output = vm["output"].as<string>();
-
-            HauppaugeDev dev(params);
-            if (!dev)
-                return -2;
-
-            USBWrapper_t usbio;
-            if (!usbio.Open(params.serial))
-                return -3;
-
-            if (!dev.Open(usbio, (params.audioCodec == HAPI_AUDIO_CODEC_AC3)))
+            if (dev.StartEncoding())
             {
-                usbio.Close();
-                return -4;
+                evtWait();
+                dev.StopEncoding();
             }
-
-            if (vm.count("duration") && vm["duration"].as<int>() > 0)
-            {
-                if (dev.StartEncoding())
-                {
-                    chrono::steady_clock::time_point start =
-                        chrono::steady_clock::now();
-                    std::chrono::seconds elapsed = chrono::seconds(0);
-                    std::chrono::seconds duration =
-                        chrono::seconds(vm["duration"].as<int>());
-                    do
-                    {
-                        elapsed = chrono::duration_cast<chrono::seconds>
-                                   (chrono::steady_clock::now() - start);
-                        PrintPosition(elapsed, duration);
-                        std::this_thread::sleep_for(std::chrono::seconds(1));
-                    }
-                    while (elapsed.count() < duration.count());
-                    cerr << "\n\n";
-                    dev.StopEncoding();
-                }
-            }
-            else
-            {
-                if (dev.StartEncoding())
-                {
-                    evtWait();
-                    dev.StopEncoding();
-                }
-            }
-
-            dev.Close();
         }
+
+        dev.Close();
     }
 
     LOG(Logger::CRIT) << "Done." << flush;

HauppaugeDev.cpp
@@ -384,7 +384,8 @@ bool HauppaugeDev::init_component(void)
         if (m_rxDev->getOutputParams(&vp) &&
             valid_resolution(vp.width, vp.height))
             break;
-        LOG(Logger::NOTICE) << "Invalid Output Params, retrying." << flush;
+        LOG(Logger::NOTICE) << "Invalid Output Params (" << vp.width << "x" << vp.height
+                            << "), retrying." << flush;
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
     if (idx == MAX_RETRY)
@@ -512,7 +513,10 @@ bool HauppaugeDev::init_hdmi(void)
 
 bool HauppaugeDev::open_file(const string & file_name)
 {
-    m_fd = open(file_name.c_str(), O_WRONLY | O_TRUNC | O_CREAT, 0666);
+    if (file_name == "stdout")
+        m_fd = 1;
+    else
+        m_fd = open(file_name.c_str(), O_WRONLY | O_TRUNC | O_CREAT, 0666);
 
     if (m_fd < 0)
     {

commit 317c24979d46022e7837684d2a0077d1795e8dde

For MythTV, do not set the output destination. Fixes a bug caused by [a88c865f0]

hauppauge2.cpp
@@ -414,7 +414,7 @@ int main(int argc, char *argv[])
 
     if (vm.count("output"))
         params.output = vm["output"].as<string>();
-    else
+    else if (!params.mythtv)
         params.output = "stdout";
 
     if (params.mythtv)